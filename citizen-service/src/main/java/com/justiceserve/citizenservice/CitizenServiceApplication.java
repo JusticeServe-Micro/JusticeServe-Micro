026e01a0afdf03e91acab9b1e6e4875ff4a2d315
Bharat-265 <BharatVenkataSiva.Katta@congnizant.com>
2026-04-07 23:16:44 +0530

    changed package name

---
+++ citizen-service/src/main/java/com/justiceserve/citizenservice/CitizenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.justiceserve.citizenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenServiceApplication.class, args);
+	}
+
+}
